63b5d8641fbe502f1aec23b72558e5b12996d714
siva1087 <120645070+siva1087@users.noreply.github.com>
2024-11-14 16:33:58 +0530

    Update Doctorslist.jsx

--- frontend/src/Pages/Doctorslist.jsx
+++ frontend/src/Pages/Doctorslist.jsx
@@ -5,6 +5,7 @@ import { Doctors } from '../assets/assets'; // Ensure this is an array of doctor
 const Doctorslist = () => {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const[showFilter,setshowFilter]=useState(false);
   const navigate = useNavigate();
 
   const applyFilter = () => {
@@ -21,9 +22,10 @@ const Doctorslist = () => {
 
   return (
     <div>
-      <p className='text-gray-600'>Browse through the Doctors' specialties.</p>
+      <p className='text-gray-600'>Browse through the Doctors' specialties.</p> 
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4  text-sm text-gray-600'>
+        <button className={`py-1 px-3  bg-primary border rounded text-sm transition-all sm:hidden${showFilter ?'bg-primary text-white':''}`} onClick={()=>setshowFilter(prev =>!prev)}>Filter</button>
+        <div className={`${showFilter? 'flex':'hidden sm:flex'}flex flex-col gap-4  text-sm text-gray-600`}>
           <p onClick={()=>speciality==='General Physician'? navigate('/doctors'):navigate('/doctors/General Physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer${speciality==="General Physician"?"bg-indigo-100 text-black":""} `}>General Physician</p>
           <p onClick={()=>speciality==='Gynecologist'? navigate('/doctors'):navigate('/doctors/Gynecologist')}  className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Gynecologist"?"bg-indigo-100 text-black":""}`} >Gynecologist</p>
           <p onClick={()=>speciality==='Dermatologist'? navigate('/doctors'):navigate('/doctors/Dermatologist') }className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer  ${speciality==="Dermatologist"?"bg-indigo-100 text-black":""} `} >Dermatologist</p>
